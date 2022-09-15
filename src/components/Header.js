90b82a180695c7b657d14a7c6e40a32c5db9e79a
prashanthdsouza <79136584+prashanthdsouza@users.noreply.github.com>
2022-09-15 16:37:25 +0530

    247

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import { useSelector, useDispatch } from 'react-redux';
 
 import classes from './Header.module.css';
-import { authActions } from '../store/index';
+import { authActions } from '../store/auth';
 
 const Header = () => {
   const dispatch = useDispatch();
-  const isAuth = useSelector(state => state.auth.isAuthenticated);
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
 
   const logoutHandler = () => {
     dispatch(authActions.logout());
@@ -14,20 +14,21 @@ const Header = () => {
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      {isAuth && <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button onClick={logoutHandler}>Logout</button>
-          </li>
-        </ul>
-      </nav>}
-      
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href='/'>My Products</a>
+            </li>
+            <li>
+              <a href='/'>My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
